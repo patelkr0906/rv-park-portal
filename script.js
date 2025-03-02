6fd45a87d0dbe60dfa3c6ccf0075ca0741376dc2
patelkr0906 <patelkr0906@gmail.com>
2025-03-02 09:32:45 -0600

    script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 // Firebase Configuration
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT_ID.appspot.com",
-    messagingSenderId: "YOUR_SENDER_ID",
-    appId: "YOUR_APP_ID"
+    apiKey: "AIzaSyC8dXLkSxci4m81P8zF9HZfLJkIN24XD7Y",
+    authDomain: "rv-park-portal.firebaseapp.com",
+    projectId: "rv-park-portal",
+    storageBucket: "rv-park-portal.firebasestorage.app",
+    messagingSenderId: "1026865868698",
+    appId: "1:1026865868698:web:b1a3e3729a5ff5625e5423"
 };
 
 // Initialize Firebase
