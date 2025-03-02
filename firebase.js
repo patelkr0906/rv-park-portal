6d1bcfb53e0f0d8229a01d7f639db7026b55cbe6
patelkr0906 <patelkr0906@gmail.com>
2025-03-02 14:08:24 -0600

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -5,12 +5,12 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase
 
 // Firebase configuration
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_AUTH_DOMAIN",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_STORAGE_BUCKET",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
+    apiKey: "AIzaSyC8dXLkSxci4m81P8zF9HZfLJkIN24XD7Y",
+    authDomain: "rv-park-portal.firebaseapp.com",
+    projectId: "rv-park-portal",
+    storageBucket: "rv-park-portal.firebasestorage.app",
+    messagingSenderId: "1026865868698",
+    appId: "1:1026865868698:web:b1a3e3729a5ff5625e5423"
 };
 
 // Initialize Firebase
